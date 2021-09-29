31bd382579ee75e806ffe1fc8ad53cac4fbb626c
AlexanderKempen <Alexander@kempenonline.com>
2021-09-29 15:26:55 +0200

    Changes

--- Introductory problems/Weird Algorithm.py
+++ Introductory problems/Weird Algorithm.py
@@ -1,11 +1,10 @@
-output = []
-while n != 1:
-    if n % 2 == 0:
-        n = n/2
-        output.append(n)
-    else:
-        n = (n*3)+1
-        output.append(n)
-
-print(output)
-
+def main(n:int):
+    output = []
+    while n != 1:
+        if n % 2 == 0:
+            n = n/2
+            output.append(n)
+        else:
+            n = (n*3)+1
+            output.append(n)
+    print(output)
