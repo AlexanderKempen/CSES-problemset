2b5be38a1b4e25ec1ba2dcce2b1fcc06134403f1
AlexanderKempen <Alexander@kempenonline.com>
2021-09-29 15:01:43 +0200

    Finished problem

--- Introductory problems/Weird Algorithm.py
+++ Introductory problems/Weird Algorithm.py
@@ -1,2 +1,11 @@
-input = 6
-print(input)
+def weirdalgorithm(x:int) -> int:
+    if x == 1:
+        return x
+    else:
+        if x % 2 == 0:
+            x = x/2
+            weirdalgorithm(x)
+
+        else:
+            x = (x*3)+1
+            weirdalgorithm(x)
