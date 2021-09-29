6c1be23ade8742c9c9b5dad8b0c62d1e650298bb
AlexanderKempen <Alexander@kempenonline.com>
2021-09-29 15:16:08 +0200

    Changes

--- Introductory problems/Weird Algorithm.py
+++ Introductory problems/Weird Algorithm.py
@@ -1,12 +1,11 @@
-def weirdalgorithm(x:int) -> int:
-    output = []
-    while x != 1:
-        if x % 2 == 0:
-            x = x/2
-            output.append(x)
-        else:
-            x = (x*3)+1
-            output.append(x)
+output = []
+while x != 1:
+    if x % 2 == 0:
+        x = x/2
+        output.append(x)
+    else:
+        x = (x*3)+1
+        output.append(x)
 
-    print(output)
+print(output)
 
